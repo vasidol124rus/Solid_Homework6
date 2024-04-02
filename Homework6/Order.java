050e7e25e3872f1a7715cb5de1e98f7a6059e186
Vasiliy Kuznetsov <vasekku25@gmail.com>
2024-04-02 15:35:06 +0700

    Order

---
+++ Homework6/Order.java
@@ -0,0 +1,13 @@
+package Homework6;
+
+class Order {
+    public void placeOrder() {
+        // размещение заказа
+    }
+}
+
+class OrderLogger {
+    public void logOrderInfo(Order order) {
+        // логирование информации о заказе
+    }
+}
